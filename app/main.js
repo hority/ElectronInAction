383bf2dd1cf9cdcc8f9dff6a34c1828ceaf9d95a
hority <info.horita@gmail.com>
2019-05-29 15:22:34 +0900

    first commit

---
+++ app/main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow = null;
+
+app.on('ready',() => {
+    console.log('Hello from Electron');
+    mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true
+        }
+    });
+    mainWindow.webContents.loadFile(`${__dirname}/index.html`);
+});
